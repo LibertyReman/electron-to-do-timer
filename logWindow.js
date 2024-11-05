25656a103a1a2b8e18d9153bc52b0e611a8bd67c
LibertyReman <libertyreman@gmail.com>
2024-11-06 07:06:10 +0900

    Windowsでログ画面のCSSが当たるように修正、ログがない場合に画面CLOSEできるように修正

--- logWindow.js
+++ logWindow.js
@@ -1,14 +1,14 @@
 // DOM読み込み完了後
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
   // クエリパラメータの取得
   const urlParams = new URLSearchParams(window.location.search);
   const logpath = urlParams.get('logpath');
 
   // ログファイルを読み込み、一番下へスクロール
-  const $iframe = document.querySelector('.js-iframe');
-  $iframe.src = logpath;
-  const iframeDocument = $iframe.contentDocument;
-  iframeDocument.documentElement.scrollTop = iframeDocument.documentElement.scrollHeight;
+  const filedata = await fetch(logpath);
+  const $logcontent = document.querySelector('.js-log-content');
+  $logcontent.textContent = await filedata.text();
+  $logcontent.scrollTop = $logcontent.scrollHeight;
 
   // CLOSEボタン押下
   document.querySelector('.js-log-close-btn').addEventListener('click', () => {
